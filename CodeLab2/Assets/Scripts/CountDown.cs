2de18bf9c461c001e23b1c26bbd1fea30052463b
Matthew Parker <maddparker@gmail.com>
2017-12-01 14:00:12 -0500

    Init Commit

---
+++ CodeLab2/Assets/Scripts/CountDown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour {
+
+	public Timer timer;
+	public TextMesh textMesh;
+	public float duration;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			timer.StartTimer(duration, UpdateClock, TimerOff);
+
+			timer.updateHandler += PrintElapsedTime;
+		}
+	}
+
+	public void PrintElapsedTime(float timeElapsed){
+		print("timeElapsed:" + timeElapsed);
+	}
+
+	private void UpdateClock(float timeElapsed){
+		textMesh.text = "" + (duration - timeElapsed);
+	}
+
+
+	private void TimerOff(){
+		textMesh.text = "TIME UP!";
+	}
+}
---
+++ CodeLab2/Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+	public delegate void UpdateHandler(float timeElapsed);
+	public event UpdateHandler updateHandler;
+
+	public delegate void EndHandler();
+	public event EndHandler endHandler;
+
+	private bool isActive = false;
+	private float currentTime = 0;
+	private float length = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(isActive){
+			currentTime += Time.deltaTime;
+
+			if(updateHandler != null){
+				updateHandler(currentTime);
+			}
+
+			if(currentTime >= length){
+				endTimer();
+			}
+		}
+	}
+
+	public void StartTimer(float time, UpdateHandler u, EndHandler e){
+		updateHandler = u;
+		endHandler = e;
+		isActive = true;
+		currentTime = 0;
+		length = time;
+	}
+
+	public void endTimer(){
+		isActive = false;
+		endHandler();
+	}
+}
